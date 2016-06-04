3aacba4c98a9401a4cdcf4bab0744834f06342de
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-06-04 10:03:28 -0300

    Criação do malloc mais básico possível

---
+++ malloc.c
@@ -0,0 +1,20 @@
+/* An horrible dummy malloc */
+
+#include <sys/types.h>
+#include <unistd.h>
+
+void* malloc(size_t size) {
+	void *p;
+
+	p = sbrk(0);
+
+	/* If sbrk fails, we return NULL */
+	if (sbrk(size) == (void*) -1) return NULL;
+
+	return p;
+}
+
+int main() {
+	//int k = (int) malloc(sizeof(int));
+	return 0;
+}
