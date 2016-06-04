af48feab0b6294147db308e2c50cf9faf7324ec8
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-06-04 10:14:22 -0300

    Adição dos metadados

--- malloc.c
+++ malloc.c
@@ -2,19 +2,30 @@
 
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
+
+typedef struct s_block *t_block;
+
+struct s_block {
+	size_t size;
+	t_block next;
+	int free;
+};
 
 void* malloc(size_t size) {
-	void *p;
+	t_block b;
 
-	p = sbrk(0);
+	b = sbrk(0);
 
 	/* If sbrk fails, we return NULL */
-	if (sbrk(size) == (void*) -1) return NULL;
+	if (sbrk(sizeof(struct s_block) + size) == (void*) -1)
+		return NULL;
 
-	return p;
+	return b + sizeof(struct s_block);
 }
 
 int main() {
-	//int k = (int) malloc(sizeof(int));
+	// int k = (int) malloc(sizeof(int));
+	// printf("%lu\n", sizeof(k));
 	return 0;
 }
